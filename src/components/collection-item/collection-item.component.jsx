4e0ba01db8550164c7911a53c2a1c66c92245d71
Jason Tran <jaycehuytran@gmail.com>
2020-08-02 20:24:09 -0700

    Fixed that colleciton item wasn't properly displaying the name and price below the images.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,13 +10,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             style = {{
                 backgroundImage: `url(${imageUrl})`
             }}
-        >
+        />
         <div className = 'collection-footer'>
             <span className='name'>{name}</span>
-            <span className='name'>{price}</span>
-        </div>
+            <span className='price'>{price}</span>
         </div>
     </div>
+
 );
 
 export default CollectionItem;
