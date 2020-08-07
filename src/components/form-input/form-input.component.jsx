5e780802696685b9016c0e1055d705533a5eebfb
Jason Tran <jaycehuytran@gmail.com>
2020-08-06 22:20:15 -0700

    Exported FormInput functional component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -22,3 +22,5 @@ const FormInput = ({handleChange, label, ...otherProps}) => (
         }
     </div>
 )
+
+export default FormInput;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component'
+
 import './sign-in.styles.scss'
 
 class SignIn extends React.Component {
@@ -32,20 +34,22 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit = {this.handleSubmit}>
-                    <input 
+                    <FormInput
                         name = 'email' 
                         type = 'email' 
-                        onChange = {this.handleChange}
+                        handleChange = {this.handleChange}
+                        label = 'email'
                         value = {this.state.email} 
                         required 
                     />
                     <label>Email</label>
 
-                    <input 
+                    <FormInput
                         name = 'password' 
                         type = 'password' 
                         value = {this.state.password} 
-                        onChange= {this.handleChange}
+                        handleChange= {this.handleChange}
+                        label = 'password'
                         required 
                     />
 
