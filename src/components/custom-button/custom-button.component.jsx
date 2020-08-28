9894ab886685bce7ecf5b600540873c98d07125e
Jason Tran <jaycehuytran@gmail.com>
2020-08-28 02:33:10 -0700

    Edited sign in page for the buttons to be more nicer.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,8 +4,10 @@ import './custom-button.styles.scss';
 
 //want to submit the form that they're in
 //pulling the children that get passed in
-const CustomButton = ({children, ...otherProps}) => (
-    <button className = 'custom-button' {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+    //if property of isGoogleSignIn is true, then render google-sign-in
+    //custom buttom is also always rendered 
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
