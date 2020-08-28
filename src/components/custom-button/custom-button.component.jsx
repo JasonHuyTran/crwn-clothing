353358e8858e562679b268fa66b53a5c3c5547a6
Jason Tran <jaycehuytran@gmail.com>
2020-08-28 01:04:27 -0700

    Did yarn add firebase

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 //want to submit the form that they're in
+//pulling the children that get passed in
 const CustomButton = ({children, ...otherProps}) => (
     <button className = 'custom-button' {...otherProps}>
         {children}
