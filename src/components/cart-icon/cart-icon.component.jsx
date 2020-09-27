ff1c5db590da98d205a3480c7fbf76d4115a7980
Jason Tran <jaycehuytran@gmail.com>
2020-09-27 00:18:11 -0700

    Now when adding items to the cart, update the number on the cart.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,20 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className = 'cart-icon' onClick = {toggleCartHidden}>
         <ShoppingIcon className = 'shopping-icon'/>
-        <span className = 'item-count'>0</span>
+        <span className = 'item-count'>{itemCount}</span>
     </div>
 )
+
+//pull the state in and get the value
+const mapStateToProps = ({cart : {cartItems }}) => ( {
+    itemCount: cartItems.reduce((accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity, 0)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
