74a4dd2c2059f2919aad0008d7964d87a04d698f
Jason Tran <jaycehuytran@gmail.com>
2020-10-01 23:38:19 -0700

    Moved unchanging state of directory component to redux folder.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -9,6 +9,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
 
 const persistConfig = {
     key: 'root',
@@ -20,7 +21,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     //property of user that points to user reducer 
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
