236bd6fb77241132499c7d8b33def77664722012
Jason Tran <jaycehuytran@gmail.com>
2020-10-03 03:13:06 -0700

    Setting up advanced routing for shop component. continued.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CollectionItem from '../../components/checkout-item/checkout-item.component'
 
+import {selectionCollection} from '../../redux/shop/shop.selectors'
+
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => (
+const CollectionPage = ({collection}) => (
     <div className = 'collection-page'>
-        <h2>CATEGORY PAGE</h2>
+        <h2>COLLECTION PAGE</h2>
     </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectionCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,27 @@
 import {createSelector} from 'reselect';
 
+//string value go to an id 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+};
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+//return createSelector, function that returns a function 
+export const selectionCollection = collectionUrlParam =>
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections.find(
+                collection => collections.id === COLLECTION_ID_MAP[collectionUrlParam]
+            )
+    )
