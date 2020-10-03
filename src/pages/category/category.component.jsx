0a8ef8b3fa06d50940b60a22e66d73ad42a5c1b9
Jason Tran <jaycehuytran@gmail.com>
2020-10-03 02:57:25 -0700

    Setting up advanced routing for shop component.

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/checkout-item/checkout-item.component'
+
+import './category.styles.scss';
+
+const CategoryPage = ({match}) => (
+    <div className = 'category'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+);
+
+export default CategoryPage;
